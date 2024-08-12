351630dceb5368edd1c64d78bcbdd7a9c1f28ac7
AnnaKarina <annapacheco203@gmail.com>
2024-08-12 12:28:06 -0300

    up

--- sadearte/src/App.jsx
+++ sadearte/src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import Cadastro from './assets/Components/Cadastro/Cadastro'
+import Cadastro from './assets/Components/Cadastro'
 import './App.css'
 
 function App() {
---
+++ sadearte/src/assets/Components/Cadastro.jsx
@@ -0,0 +1,42 @@
+import React from 'react'
+import { useState } from 'react';
+import "./Cadastro.css";
+
+const Cadastro = () => {
+
+   const [username, setUsername] = useState("");
+   const [password, setPassword] = useState("");
+
+   const handleSubmit = (event) => {
+    event.preventDefault();
+    
+    console.log("Envio");
+   };
+
+
+  return (
+    <div className='Conteiner'>
+      <form onSubmit={handleSubmit}>
+        <div><input type="username" placeholder='Nome de usuário'/></div>
+        <div><input type="email" placeholder='Email'/></div>
+        <div><input type="password" placeholder='Senha'/></div>
+        <div> <input type="password" placeholder='Confirme sua senha'/></div>
+        <div className='recall-forget'>
+            <label htmlFor="">
+                <input type="checkbox"/>
+                Lembre de mim
+            </label> <br />
+            <link rel="stylesheet" href="./Cadastro.jsx" />
+        </div>
+
+        <button>Entrar</button>
+
+        <div className="signup-link">
+            <p>Já possuí uma conta? <a href="#">Login</a></p>
+        </div>
+      </form>
+    </div>
+  )
+}
+
+export default Cadastro
